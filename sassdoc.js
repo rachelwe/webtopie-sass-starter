eae0c7132896cd560e9a5124ff1b5fe20ac61222
Rachel P <pellin.rachel@gmail.com>
2021-03-05 22:14:01 +0100

    initial commit

---
+++ sassdoc.js
@@ -0,0 +1,8 @@
+var sassdoc = require('sassdoc');
+
+sassdoc('sass', { verbose: true })
+  .then(function () {
+    console.log('Your documentation has been generated!');
+  }, function (err) {
+    console.error(err);
+  });
