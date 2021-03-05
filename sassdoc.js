d4908563b897d3029f79fe5a325df160420794fb
Rachel P <pellin.rachel@gmail.com>
2021-03-05 22:17:26 +0100

    move sassdoc to docs folder

--- sassdoc.js
+++ sassdoc.js
@@ -1,6 +1,6 @@
 var sassdoc = require('sassdoc');
 
-sassdoc('sass', { verbose: true })
+sassdoc('sass', { verbose: true, dest: './docs' })
   .then(function () {
     console.log('Your documentation has been generated!');
   }, function (err) {
